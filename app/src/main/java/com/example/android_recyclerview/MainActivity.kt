cb98ee09912ad50e2c5e2fe81f3ec12068c9e2e4
avidaldo <avd.formacion@gmail.com>
2022-04-25 20:09:02 +0200

    ListViewNofunciona

---
+++ app/src/main/java/com/example/android_recyclerview/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.android_recyclerview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.android_recyclerview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
+        navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+    }
+
+    override fun onSupportNavigateUp() =
+        navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+
+}
---
+++ app/src/main/java/com/example/android_recyclerview/MainFragment.kt
@@ -0,0 +1,84 @@
+package com.example.android_recyclerview
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.android_recyclerview.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    /***********************************************************/
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.listaEjemplos
+            .apply {
+                adapter = ArrayAdapter.createFromResource(
+                    requireActivity(),
+                    R.array.ejemplos,
+                    android.R.layout.simple_spinner_dropdown_item
+                )
+            }
+            .apply {
+                onItemSelectedListener = listener
+            }
+
+
+    }
+
+
+    object listener : AdapterView.OnItemSelectedListener {
+        override fun onItemSelected(
+            parent: AdapterView<*>,
+            view: View,
+            position: Int,
+            id: Long,
+        ) {
+            Log.i("INFO","""OnItemSelectedListener:
+                parent.selectedItem = ${parent.selectedItem}
+                parent?.getItemAtPosition(position) = ${parent.getItemAtPosition(position)}
+                (view as TextView).text = ${(view as TextView).text}
+                position = $position
+                id = $id
+                """.trimIndent())
+
+            parent.findNavController().navigate(
+                when (position) {
+                    0 -> MainFragmentDirections.actionFirstFragmentToSecondFragment()
+/*                                1 -> SpinnersFragmentDirections.actionSpinnersFragmentToEj02OnItemSelectedFragment()
+                                2 -> SpinnersFragmentDirections.actionSpinnersFragmentToEj03FromArrayFragment()
+                                3 -> SpinnersFragmentDirections.actionSpinnersFragmentToEj04FromResourceFragment()
+                                4 -> SpinnersFragmentDirections.actionSpinnersFragmentToEj05FromArrayListFragment()
+                                5 -> SpinnersFragmentDirections.actionSpinnersFragmentToEj06Fragment()*/
+                    else -> throw Exception("Ejemplo no existente")
+                }
+            )
+        }
+
+        override fun onNothingSelected(parent: AdapterView<*>?) {}
+    }
+}
